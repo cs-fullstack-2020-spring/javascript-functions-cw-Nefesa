0f70cd536e19e1ecb5acf25b896a46db51b70976
Nefesa <nefesa@hotmail.com>
2020-02-21 14:47:08 -0600

    firsttry

---
+++ functions.js
@@ -0,0 +1,68 @@
+
+
+// # JavaScript Functions CW
+
+// ### Problem 1
+
+// Create a ```printNumbers``` function to print integers from -25 to 20.
+// ?created a function
+// function someNumber()
+// {
+//     // printed for -25 to 20 
+//     for  (i=-25; i<=20; i++)
+//     {
+//         console.log(i);
+//     }
+// }
+// someNumber();
+
+
+// ### Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+// function newName (oddnam1,oddnam2)
+// {
+//     console.log(`My first name is ${oddname1}\n My last name is 
+//     ${oddnam2}`);{
+//         return (oddnam1+oddnam2);
+//         let firstName = "Nefesa"
+//         let lastName = "Walker"
+//         console.log(oddname1+ oddnam2);
+//     }
+
+// }
+
+
+// ### Problem 3
+// Create a function in your program that counts from 0 to [NUMBER]
+
+function infinateNumber()
+{
+    for  (i=0; i=50; i++)
+    {
+        console.log(i);
+    }
+}
+infinateNumber();
+
+// ### Problem 4
+// Create a function called checkPassword. Send two string variables to the checkPassword function to check if the strings are equal. Return true if they are equal and false if they are not equal. Print the function's return value.
+
+
+
+
+// ### Problem 5
+// Create a ```sumOf3Numbers``` function that will print out the sum of the three numbers using the starting code below:
+// ```javascript
+// var number1 = 0;
+// var number2 = 0;
+// var number3 = 0;
+
+// sumOf3Numbers(number1, number2, number3);
+// ```
+
+// ### Challenge
+// Create 4 functions called add, subtract, multiply, and divide. Create them to allow a user to perform the name of the function to the two numbers and return the result.
+
+
+
